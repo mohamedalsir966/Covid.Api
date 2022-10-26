f9d15b9961af3c7457b124fa370823704443f214
Mohamed Alsir <mohamedalsir966@gmail.com>
2022-10-26 13:07:30 +0400

    Commit

--- Service/Mapper/PatientProfile.cs
+++ Service/Mapper/PatientProfile.cs
@@ -16,8 +16,7 @@ namespace Service.Mapper
                    .ForMember(from => from.Vaccin, to => to.MapFrom(value => value.PatientVaccin))
                    .ForMember(from => from.IsVaccinated, to => to.MapFrom(value => value.PatientVaccin.Count() >= value.PatientVaccin.FirstOrDefault().Vaccin.TotalNumOfDose))
                    ;
-            //CreateMap<PatientVaccin, VaccinDto>()
-            //    .ForMember(from => from.Name, to => to.MapFrom(value => value.Vaccin.Name));
+            
             CreateMap<Patient, CreateNewPatientCommand>().ReverseMap();
 
         }
